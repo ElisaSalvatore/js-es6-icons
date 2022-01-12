e26ea57b1a99192f4bff342c4ca973112439d5d2
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-12 19:55:39 +0100

    milestone 3 da finire

--- js/script.js
+++ js/script.js
@@ -116,15 +116,30 @@ const iconsList = [
 // Milestone 1
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, 
 // in cui è presente il nome dell’icona e l’icona stessa.
-const outputIcons = document.getElementById('output-icons');
+const containerIcons = document.querySelector('.container-icons');
 
 iconsList.forEach(function(element) {
     const createBox = document.createElement('div');
     createBox.className = `box ${element.family} ${element.prefix}${element.name}`;
+
+    // Milestone 2
+    // Ciascuna icona ha una proprietà “color”: 
+    // utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
     createBox.classList.add(element.color);
-    outputIcons.append(createBox);
+    containerIcons.append(createBox);
 });
 
-// Milestone 2
-// Ciascuna icona ha una proprietà “color”: 
-// utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone 
+//(animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, 
+//visualizzare solamente le icone corrispondenti.
+
+// PROVA FILTER - NON FUNZIONA 
+const animalsArrayFilter = iconsList.filter(function(type) {
+	const firstChar = type[0];
+	if (firstChar === 'a') {
+		return true;
+	} 
+	return false;
+})
+console.log(animalsArrayFilter);
