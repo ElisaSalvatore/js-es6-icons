9f3ba4986818deae5b166a16d726eedf87bd52e5
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-12 16:01:05 +0100

    set forEach using second method

--- js/script.js
+++ js/script.js
@@ -4,122 +4,127 @@ const iconsList = [
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: 'orange'
+		color: 'blue'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'purple'
+		color: 'orange'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'purple'
+		color: 'orange'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'purple'
+		color: 'orange'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: 'purple'
+		color: 'orange'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'purple'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'purple'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'purple'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: 'blue'
+		color: 'purple'
 	}
 ];
 
 // Milestone 1
 // Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, 
 // in cui è presente il nome dell’icona e l’icona stessa.
-const outputIcons = document.querySelector('.output-icons');
+const outputIcons = document.getElementById('output-icons');
 
-iconsList.forEach((element) => {
-    outputIcons.innerHTML += `<div class="box"> <i class="${element.family} ${element.prefix}${element.name}"> ${element.name}</i> </div>`;
+iconsList.forEach(function(element) {
+    const createBox = document.createElement('div');
+    createBox.className = `box ${element.family} ${element.prefix}${element.name}`;
+    createBox.classList.add(element.color);
+    outputIcons.append(createBox);
 });
 
-
+// Milestone 2
+// Ciascuna icona ha una proprietà “color”: 
+// utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
