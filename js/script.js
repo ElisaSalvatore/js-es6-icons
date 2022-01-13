29612c02069d12a86591d8f58bb50c0b38ae0969
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-01-13 18:46:34 +0100

    Add JS: line 135 (function) and 153, 169 (selectElement)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+// Milestone 1
+// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, 
+// in cui è presente il nome dell’icona e l’icona stessa.
+
 const iconsList = [
 	{
 		name: 'cat',
@@ -113,44 +117,62 @@ const iconsList = [
 	}
 ];
 
-// Milestone 1
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, 
-// in cui è presente il nome dell’icona e l’icona stessa.
-
 // Milestone 2
 // Ciascuna icona ha una proprietà “color”: 
 // utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
-// Milestone 3
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone 
-//(animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, 
-//visualizzare solamente le icone corrispondenti.
-
 function createBoxIcons(element){
 	return `
-		<div class="box ${element.color}">
-			<i class="${element.family} ${element.prefix}${element.name}">
-				<div class="printed-name">${element.name}</div>
-			</i>
+		<div class="box d-flex flex-column justify-content-center align-items-center ${element.color}">
+			<i class="${element.family} ${element.prefix}${element.name}"></i>
+			<div class="printed-name">${element.name.toUpperCase()}</div>
 		</div> 
 	`
 };
 
 const containerIcons = document.querySelector('.container-icons');
 
-iconsList.forEach((element) => {
-	containerIcons.innerHTML += createBoxIcons(element);
+function printIcons(containerIcons, iconsList) {
+	iconsList.forEach((icon) => {
+		containerIcons.innerHTML += createBoxIcons(icon);
+	});
+};
+printIcons(containerIcons, iconsList);
+
+// Milestone 3
+// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone 
+//(animal, vegetable, user). Quando l’utente seleziona un tipo dalla select, 
+//visualizzare solamente le icone corrispondenti.
+
+//UTILIZZARE SelectElement PER SELEZIONARE GLI ELEMENTI 
+//UTILIZZARE .filter PER FILTRARE LE ICONE (.value)
+const selectElementType = document.querySelector('.filter-type');
+const selectElementColor = document.querySelector('.filter-color');
+
+// SELECT PER TYPE
+selectElementType.addEventListener('change', function(event) {
+	containerIcons.innerHTML = '';
+	const filtredIcons = iconsList.filter(function(icon) {
+		// if (selectElementType.value === icon.type || selectElementType.value === 'all'){
+		// 	return true;
+		// }
+		// return false;
+		return selectElementType.value === icon.type || selectElementType.value === 'all';
+	})
+
+	printIcons(containerIcons, filtredIcons);
+	// console.log(selectElementType.value)
+	// console.log(filtredIcons)
 });
 
-
-//UTILIZZARE SELECT ELEMENT - DA FINIRE
-const selectElement = document.querySelector('.filter');
-
-selectElement.addEventListener('change', (event) => {
-	const filtredIcons = iconsList.filter((element) => {
-		if (element.type === 'a') {
-			return true;
-		};
-		return false;
+// SELECT PER COLOR
+selectElementColor.addEventListener('change', function(event) {
+	containerIcons.innerHTML = '';
+	const filtredIcons = iconsList.filter(function(icon) {
+		return selectElementColor.value === icon.color || selectElementColor.value === 'all';
 	})
+
+	printIcons(containerIcons, filtredIcons);
+	// console.log(selectElementColor.value)
+	// console.log(filtredIcons)
 });
